6e6afa6ff4333dd34129d503eb2507321af16684
annora1304 <naylaannora@gmail.com>
2025-04-30 15:19:48 +0700

    jobsheet stack

---
+++ StackTugasMahasiswa12.java
@@ -0,0 +1,50 @@
+public class StackTugasMahasiswa12 {
+    Mahasiswa12[] stack;
+    int top;
+    int size;
+
+    public StackTugasMahasiswa12(int size) {
+        this.size = size;
+        stack = new Mahasiswa12[size];
+        top = -1;
+    }
+    public boolean isFull() {
+        return top == size - 1;
+    }
+    public boolean isEmpty() {
+        return top == -1;
+    }
+    public void push(Mahasiswa12 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+        }
+    }
+    
+    public Mahasiswa12 pop() {
+        if (!isEmpty()) {
+            Mahasiswa12 mhs = stack[top];
+            top--;
+            return mhs;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang bisa diambil.");
+            return null;
+        }
+    }
+    public Mahasiswa12 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang bisa dilihat.");
+            return null;
+        }
+    }
+    public void print() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println("Nama: " + stack[i].nama + ", NIM: " + stack[i].nim + ", Kelas: " + stack[i].kelas + ", Nilai: " + stack[i].nilai);
+        }
+        System.out.println("");
+    }
+}
